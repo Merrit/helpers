811a5a519c29829a31e5553586e7425147e87ac1
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2022-06-23 16:44:50 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Collection of helpers to make Dart & Flutter development easier.
---
+++ lib/helpers.dart
@@ -0,0 +1,7 @@
+/// Collection of helpers to make Dart & Flutter development easier.
+library helpers;
+
+export 'src/form_factor.dart';
+export 'src/platform.dart';
+export 'src/show_context_menu.dart';
+export 'src/text_editing_controller.dart';
