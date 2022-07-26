c996ae9e18832465c4deb3f24b710d5f5b280af0
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2022-07-26 14:41:08 -0400

    Add assetToTempDir

---
+++ lib/src/asset_to_temp_dir.dart
@@ -0,0 +1,44 @@
+import 'dart:io';
+
+import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
+
+/// Converts an asset into an accessible file in the temp directory.
+///
+/// Takes a String [path] which is the same used to identify the asset.
+///
+/// For example, if you have assets specified like this:
+///
+/// ```yaml
+/// assets:
+///   - assets/icons/
+///   - assets/amazingfont.tiff
+///   - images/sparkle.png
+/// ```
+///
+/// You can have an asset converted into a [File] by:
+///
+/// ```dart
+/// File coolIcon = await assetToTempDir('assets/icons/cool_icon.png');
+/// ```
+///
+/// ```dart
+/// File amazingFont = await assetToTempDir('assets/amazingfont.tiff');
+/// ```
+///
+/// ```dart
+/// File sparkleImage = await assetToTempDir('images/sparkle.png');
+/// ```
+Future<File> assetToTempDir(String path) async {
+  final String separator = (Platform.isWindows) ? '\\' : '/';
+  final String assetName = path.split(separator).last;
+  final assetBytes = await rootBundle.load(path);
+  final tempDir = await getTemporaryDirectory();
+  final assetFile = File('${tempDir.path}/$assetName');
+  return await assetFile.writeAsBytes(
+    assetBytes.buffer.asUint8List(
+      assetBytes.offsetInBytes,
+      assetBytes.lengthInBytes,
+    ),
+  );
+}
