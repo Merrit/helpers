65aa9ac0e25138b2af31395fb91499f34cbe175b
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2023-02-09 15:42:08 -0500

    fix: testing variable returns false on web

--- lib/src/testing.dart
+++ lib/src/testing.dart
@@ -1,4 +1,15 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
+
 /// True if the running environment is a unit test.
-final bool testing = Platform.environment.containsKey('FLUTTER_TEST');
+final bool testing = _checkTesting();
+
+// Check if running in a test environment.
+bool _checkTesting() {
+  if (kIsWeb) {
+    return false;
+  } else {
+    return Platform.environment.containsKey('FLUTTER_TEST');
+  }
+}
