2d3cd10b6d59e02fca5373fd6d3662d262017f38
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2023-02-23 13:54:00 -0500

    refactor: make ReleaseNotes plain data class
    Causing issues importing freezed class in other package

--- lib/src/release_notes/src/models/release_notes.dart
+++ lib/src/release_notes/src/models/release_notes.dart
@@ -1,21 +1,52 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
-
-part 'release_notes.freezed.dart';
-
 /// A model representing the release notes for a specific version.
-@freezed
-class ReleaseNotes with _$ReleaseNotes {
-  factory ReleaseNotes({
-    /// The version of the app.
-    required String version,
+class ReleaseNotes {
+  const ReleaseNotes({
+    required this.version,
+    required this.date,
+    required this.notes,
+    required this.fullChangeLogUrl,
+  });
 
-    /// The date the version was released.
-    required String date,
+  final String version;
+  final String date;
+  final String notes;
+  final String fullChangeLogUrl;
 
-    /// The release notes for the version.
-    required String notes,
+  ReleaseNotes copyWith({
+    String? version,
+    String? date,
+    String? notes,
+    String? fullChangeLogUrl,
+  }) {
+    return ReleaseNotes(
+      version: version ?? this.version,
+      date: date ?? this.date,
+      notes: notes ?? this.notes,
+      fullChangeLogUrl: fullChangeLogUrl ?? this.fullChangeLogUrl,
+    );
+  }
 
-    /// The URL to the full release notes.
-    required String fullChangeLogUrl,
-  }) = _ReleaseNotes;
+  @override
+  String toString() {
+    return 'ReleaseNotes(version: $version, date: $date, notes: $notes, fullChangeLogUrl: $fullChangeLogUrl)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is ReleaseNotes &&
+        other.version == version &&
+        other.date == date &&
+        other.notes == notes &&
+        other.fullChangeLogUrl == fullChangeLogUrl;
+  }
+
+  @override
+  int get hashCode {
+    return version.hashCode ^
+        date.hashCode ^
+        notes.hashCode ^
+        fullChangeLogUrl.hashCode;
+  }
 }
